4aaa615bf8af2e6396a252cb93359d1fe50711fc
hyuc@bupt.edu.cn <heyuanchuan1991@gmail.com>
2014-03-25 23:31:29 +0800

    add staic link list.

---
+++ StaticLinkList.cpp
@@ -0,0 +1,39 @@
+#define MAXSIZE 1000
+typedef struct
+{
+	ElemType data;
+	int cur;
+}Component,StaticLinkList[MAXSIZE];
+Status InitList(StaticLinkList space)
+{	
+	for(i=0;i<MAXSIZE-1;i++)
+		space[i].cur=i+1;
+	space[MAXSIZE-1].cur=0;	
+	//数组最后一个元素的cur用来存放第一个插入元素的下标，相当于头结点
+	return OK;
+}
+int Malloc_SLL(StaticLinkList apace)
+{
+	int i =space[0].cur;
+	if(space[0].cur)
+		space[0].cur=space[i].cur;
+	return i;
+}
+Status ListInsert(StaticLinkList L,int i,ElemType e)
+{
+	int  j,k,l;
+	k=MAXSIZE-1;
+	if(i<1 || i>ListLength(L)+1)
+		return ERROR;
+	j=Malloc_SSL(L);
+	if(j)
+	{
+		L[j].data =e;
+		for(l=1;l<=i-1;i++)	//找到第i个元素之前的位置
+			k=L[k].cur;
+		L[j].cur =L[k].cur;
+		L[k].cur=j;
+		return OK;
+	}
+	return ERROR;
+}

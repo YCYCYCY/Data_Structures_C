a12c475a8a26406aefb30ea5c6197167cc805c75
heyuanchaun <heyuanchuan1991@gmail.com>
2014-08-19 21:27:50 +0800

    modify chapter_7_sort

--- chapter_7_sort.cpp
+++ chapter_7_sort.cpp
@@ -86,3 +86,82 @@ void shellSort(int *num,int size)
 	quickSort(num,begin,j-1);
 	quickSort(num,i;end);
 }
+
+int partition(SqList &L,int low,int high)
+{
+	L.r[0]=L.r[low];
+	pivotkey=L.r[low].key;
+	while(low<high)
+	{
+		while(low<high && L.r[high]>=pivotkey)
+			--high;
+		L.r[low]=L.r[high];
+		while(low<high && L.r[low].key<=pivotkey)
+			++low;
+		L.r[high]=L.r[low];
+	}
+	L.r[low]=L.r[0];
+	return low;
+}
+void QSort(SqList &L,int low,int high)
+{
+	if(low<high)
+	{
+		pivotloc=Parition(L,low,high);
+		QSort(L,low,pivotloc-1);
+		QSort(L,pivotloc+1,high);
+	}
+}
+void HeapAdjust(HeapType &H,int s,int m)
+{
+	rc=H.r[s];
+	for(j=2*s;j<=m;j*=2)
+	{
+		if(j<m && H.r[j].key<H[j+1].key)
+			j++;
+
+		if(rc.key>=H.r[j].key)
+			break;
+		H.r[s]=H.r[j];
+		s=j;
+	}
+	H.r[s]=rc;
+}
+void HeapSort(HeapType &H)
+{
+	for(i=H.length/2;i>0;--i)
+		HeapAdjust(H,I,H.length);
+	for(i=H.length;i>1;--i)
+	{
+		H.r[1]=H.r[i];
+		HeapAdjust(H,1,i-1);
+	}
+}
+//merging sort
+void MergeArray(int *,int,int,int,int *);
+void MergeSort(int *data,int first,int last,int *temp)
+{
+  if(first<last)
+  {
+    int mid=(first+last)/2;
+    MergeSort(data,first,mid,temp);
+    MergeSort(data,mid+1,last,temp);
+    MergeArray(data,first,mid,last,temp);
+  }
+}
+void MergeArray(int *data,int first,int mid,int last,int *temp)
+{
+  int i=first,j=mid+1,k=first;
+  while(i<=mid && j<=last)
+  {
+    if(data[i]<data[j])
+      temp[k++]=data[i++];
+    else
+      temp[k++]=data[j++];
+  }
+  while(i<=mid)
+    temp[k++]=data[i++];
+  while(j<=last)
+    temp[k++]=data[j++];
+}
+						
